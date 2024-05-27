d68a23a45b96be5b005c67f047baaa81300533b6
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-05-27 15:12:03 +0200

    Implement Lagrange interpolation

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ def read_data(file_path):
     return data['distance'].values, data['elevation'].values
 
 
+def lagrange_interpolation(x, y, x_new):
+    n = len(x)
+    m = len(x_new)
+    y_interp = np.zeros(m)
+
+    for i in range(m):
+        p = 0
+        for j in range(n):
+            L = 1
+            for k in range(n):
+                if k != j:
+                    L *= (x_new[i] - x[k]) / (x[j] - x[k])
+            p += y[j] * L
+        y_interp[i] = p
+
+    return y_interp
+
+
 def main():
     pass
 
